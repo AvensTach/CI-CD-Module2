4d154dea4d1d10a4c0438882855502b9e062da79
artur <artur.tymchuk@gmail.com>
2026-05-22 14:00:23 +0300

    Changed settings.py to use .env and add main view to display 5 latest recipes

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,9 @@
+from django.shortcuts import render
+from django.db.models import Count
+from .models import Recipe, Category # Переконайтеся, що імпортували моделі
 
+def main(request):
+    """Відображає останні 5 створених рецептів."""
+    # Припускаємо, що модель Recipe має поле created_at
+    recent_recipes = Recipe.objects.all().order_by('-created_at')[:5]
+    return render(request, 'main.html', {'recent_recipes': recent_recipes})
